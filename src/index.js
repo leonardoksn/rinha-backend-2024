39dc168ed6a203b9488e8f059f020cf9f086b744
leonardoksn <leonardo.ksn@gmail.com>
2024-02-08 21:22:58 -0300

    feat: criado rota de transação

--- src/index.js
+++ src/index.js
@@ -3,12 +3,44 @@ import express from "express";
 
 const { PORT } = process.env
 
-const app = express()
+const app = express();
+app.use(express.json({ limit: "50mb" }));
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+
+app.post("/clientes/:id/transacoes", async (req, res) => {
+
+    const id = req.params.id
+
+    const isFind = await findClient(id)
+    if (!isFind) {
+        return res.status(404).send("Client not found")
+    }
+
+    const { body } = req;
+    console.log(body)
+    //Verify body.valor is int that is greater than 0
+    if (!Number.isInteger(body.valor) || body.valor <= 0) {
+        return res.status(404).send("Body not found")
+    }
+    //Verify body.tipo is string "c" or "d"
+    if (body.tipo !== "c" && body.tipo !== "d") {
+        return res.status(404).send("Body not found")
+    }
+    //Verify body.descricao is string that hava 1 a 10 caracters
+    if (body.descricao.length < 1 || body.descricao.length > 10) {
+        return res.status(404).send("Body not found")
+    }
+
+    res.status(200).send({
+        "limite": 100000,
+        "saldo": -9098
+    })
 })
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+async function findClient(id) {
+    return id
+}
