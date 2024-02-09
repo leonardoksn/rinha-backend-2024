02999f9158c33fd546a36bc69660ceb35623d6d9
leonardoksn <leonardo.ksn@gmail.com>
2024-02-08 21:58:03 -0300

    feat: criado tabela de transação

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import "dotenv/config";
 import express from "express";
-
-const { PORT } = process.env
+import db from "./db.js";
+const { PORT } = process.env;
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
-
 
 app.post("/clientes/:id/transacoes", async (req, res) => {
 
@@ -40,6 +39,27 @@ app.post("/clientes/:id/transacoes", async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const criarTabela = async () => {
+    try {
+        const query = `
+        CREATE TABLE IF NOT EXISTS transacoes (
+          id SERIAL PRIMARY KEY,
+          valor INT NOT NULL,
+          tipo VARCHAR(1) NOT NULL,
+          descricao VARCHAR(10) NOT NULL
+        )
+      `;
+        await db.query(query);
+        console.log('Tabela criada com sucesso!');
+    } catch (error) {
+        console.error('Erro ao criar tabela:', error);
+    } finally {
+        await db.end();
+    }
+};
+
+criarTabela();
 
 async function findClient(id) {
     return id
