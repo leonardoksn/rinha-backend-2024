ff5e760001632f82e1863d5ef6b123c3a1a87aa9
leonardoksn <leonardo.ksn@gmail.com>
2024-02-09 14:20:07 -0300

    feat: rota de transação salvando dados no banco

--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ app.post("/clientes/:id/transacoes", async (req, res) => {
 
     const id = req.params.id
 
-    const isFind = await findClient(id)
-    if (!isFind) {
+    const costumer = await findClient(id)
+    if (!costumer) {
         return res.status(404).send("Client not found")
     }
 
     const { body } = req;
-    console.log(body)
     //Verify body.valor is int that is greater than 0
     if (!Number.isInteger(body.valor) || body.valor <= 0) {
         return res.status(404).send("Body not found")
@@ -29,11 +28,37 @@ app.post("/clientes/:id/transacoes", async (req, res) => {
     if (body.descricao.length < 1 || body.descricao.length > 10) {
         return res.status(404).send("Body not found")
     }
+    // const { rows } = await db.query("INSERT INTO transacoes (valor, tipo, descricao) VALUES ($1, $2, $3) RETURNING *", [body.valor, body.tipo, body.descricao]);
+    const newBalance = costumer.saldo - body.valor;
 
+    if (body.tipo === "d") {
+        const isNegative = (newBalance + costumer.limite) < 0;
+        if (isNegative) {
+            return res.status(422).send()
+        }
+    }
+    const client = await db.connect()
+
+    try {
+
+        await client.query('BEGIN')
+
+        await client.query("INSERT INTO transacoes (valor, tipo, descricao) VALUES ($1, $2, $3)", [body.valor, body.tipo, body.descricao]);
+
+        await client.query("UPDATE clientes SET saldo = $1 WHERE id = $2", [newBalance, id]);
+
+        await client.query('COMMIT')
+    } catch (error) {
+
+        await client.query('ROLLBACK')
+        return res.status(500).send(error.message)
+    }
     res.status(200).send({
-        "limite": 100000,
-        "saldo": -9098
+        "limite": costumer.limite,
+        "saldo": newBalance
     })
+
+
 })
 
 app.listen(PORT, () => {
@@ -42,5 +67,7 @@ app.listen(PORT, () => {
 
 
 async function findClient(id) {
-    return id
+    const { rows } = await db.query("SELECT LIMITE, SALDO FROM clientes WHERE id = $1", [id]);
+    return rows[0]
+
 }
