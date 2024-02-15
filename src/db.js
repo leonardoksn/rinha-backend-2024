06a72512fd56e4a0f7551a8c6b256ab6286f9db3
leonardoksn <leonardo.ksn@gmail.com>
2024-02-15 09:57:22 -0300

    config: removido volume do banco

--- src/db.js
+++ src/db.js
@@ -36,6 +36,18 @@ pool.once('connect', () => {
         cliente_id INT NOT NULL,
         FOREIGN KEY (cliente_id) REFERENCES clientes(id)
     );
+    DO $$
+    BEGIN
+        INSERT INTO clientes (nome, limite)
+        VALUES
+            ('o barato sai caro', 1000 * 100),
+            ('zan corp ltda', 800 * 100),
+            ('les cruders', 10000 * 100),
+            ('padaria joia de cocaia', 100000 * 100),
+            ('kid mais', 5000 * 100);
+        
+    END;
+    $$;
 
     `)
 
