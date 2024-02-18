a5573f487c18bbd590fa2e5ec28e9b5c3e842581
leonardoksn <leonardo.ksn@gmail.com>
2024-02-18 15:06:51 -0300

    refactor: colocado criação e inserção de valores iniciais no banco através de um arquivo .sql ao invés da propria api fazer isso

--- src/db.js
+++ src/db.js
@@ -7,50 +7,13 @@ const pool = new pg.Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_DB,
     password: POSTGRES_PASSWORD,
-    port: 5432,
+    port: 5432
 });
 
 pool.on('error', connect);
 
 pool.once('connect', () => {
     log('Conectado ao postgres');
-
-    return pool.query(`
-
-    -- Criar tabela clientes
-    CREATE TABLE IF NOT EXISTS clientes (
-        id SERIAL PRIMARY KEY,
-        limite INT NOT NULL,
-        saldo INT DEFAULT 0,
-        nome VARCHAR(50) NOT NULL
-    );
-    
-    
-    -- Criar tabela transacoes
-    CREATE TABLE IF NOT EXISTS transacoes (
-        id SERIAL PRIMARY KEY,
-        valor INT NOT NULL,
-        tipo VARCHAR(1) NOT NULL,
-        realizada_em TIMESTAMP NOT NULL DEFAULT NOW(),
-        descricao VARCHAR(10) NOT NULL,
-        cliente_id INT NOT NULL,
-        FOREIGN KEY (cliente_id) REFERENCES clientes(id)
-    );
-    DO $$
-    BEGIN
-        INSERT INTO clientes (nome, limite)
-        VALUES
-            ('o barato sai caro', 1000 * 100),
-            ('zan corp ltda', 800 * 100),
-            ('les cruders', 10000 * 100),
-            ('padaria joia de cocaia', 100000 * 100),
-            ('kid mais', 5000 * 100);
-        
-    END;
-    $$;
-
-    `)
-
 })
 
 async function connect() {
@@ -69,4 +32,4 @@ connect();
 
 export default pool
 
-   
+
